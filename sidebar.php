31e09dcf0941c4ea752fc91612b23f7e88a3710e
Gabe Casalett <gcasalett@gmail.com>
2015-04-30 17:58:29 -0400

    I lost track

--- sidebar.php
+++ sidebar.php
@@ -2,8 +2,8 @@
   <aside class="widget quick-links">
     <h1 class="widget-title">Quick Links</h1>
     <ul>
-      <li><a href="http://ncelectionconnection.com/registering-to-vote/voter-registration-faq/#three"><span class="icon icon-clipboard"></span> Check Your Registration</a></li>
-      <li><a href="http://www.ncelectionconnection.com/downloads/LocalElectionTerms.xls"><span class="icon icon-office"></span> Chart of NC City Elections</a></li>
+      <li><a href="http://www.ncvoter.org/registering-to-vote/voter-registration-faq/#three"><span class="icon icon-clipboard"></span> Check Your Registration</a></li>
+      <li><a href="http://www.ncvoter.org/downloads/LocalElectionTerms.xls"><span class="icon icon-office"></span> Chart of NC City Elections</a></li>
     </ul>
   </aside>
 
